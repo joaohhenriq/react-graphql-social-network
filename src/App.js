ac1d1a299667137bcaccf2f5cc41257d396fcb7d
joaoh <joao_camargo10@hotmail.com>
2020-01-23 20:39:44 -0300

    Navbar component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom'
+
+import 'semantic-ui-css/semantic.min.css'
 import './App.css';
+
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Register from './pages/Register'
+import MenuBar from './component/MenuBar'
 
 function App() {
   return (
-    <div className="App">
-      <h1>aaaa</h1>
-    </div>
+    <Router>
+      <div className='ui container'>
+        <MenuBar />
+        <Route exact path='/' component={Home} />
+        <Route exact path='/login' component={Login} />
+        <Route exact path='/register' component={Register} />
+      </div>
+    </Router>
   );
 }
 
